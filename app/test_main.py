5c88f3c84ea6e520a636ea9acc615826b479daa1
Oleh <musiychuk09@gmail.com>
2024-04-24 18:23:45 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,22 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        ("look", False),
+        ("Adam", False),
+        ("COveR", True),
+        ("", True),
+    ],
+    ids=[
+        "If letter in word repeat, return False",
+        "The value is not case sensitive",
+        "The value is not case sensitive",
+        "If string is empty, return True"
+    ]
+)
+def test_is_isogram(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
